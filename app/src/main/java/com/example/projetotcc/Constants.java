b67429971f853e70e36eccb869abeaa47e68350f
NicoleAmorim <Nick.Amorim004@gmail.com>
2020-10-15 22:25:05 -0300

    Tela Perfil

---
+++ app/src/main/java/com/example/projetotcc/Constants.java
@@ -0,0 +1,11 @@
+package com.example.projetotcc;
+
+public class Constants {
+   public static final String CadastrarServicoUrl = "http://192.168.0.144/ApisAndroidPHP/CadastrarServico.php";
+   public static final String CadastrarUsuarioUrl = "http://192.168.0.144/ApisAndroidPHP/CadastrarUsuario.php";
+   public static final String LoginUsuarioUrl = "http://192.168.0.144/ApisAndroidPHP/LoginUsuario.php";
+   public static final String SelecionarPrimeiroServicoUrl = "http://192.168.0.144/ApisAndroidPHP/SelecionarPrimeiroServico.php";
+   public static final String SelecionarServicoUrl = "http://192.168.0.144/ApisAndroidPHP/SelecionarServico.php";
+   public static final String SelecionarUltimoServicoUrl = "http://192.168.0.144/ApisAndroidPHP/SelecionarUltimoServico.php";
+   public static final String SelecionarUsuarioServicoUrl = "http://192.168.0.144/ApisAndroidPHP/SelecionarServicoByUserId.php";
+}
